14544421d8d0c7319db783519db392dd1cb3d89d
Luis Gerhorst <luis@luisgerhorst.de>
2013-03-04 16:51:47 +0100

    Ember.js routes for posts/*

--- js/load-config.js
+++ js/load-config.js
@@ -1,9 +1,7 @@
-var couchdb;
-
 $.ajax({
 	url: 'config.json'
 }).done(function (data) {
-	console.log('Received configuration.', data);
-	couchdb = new CouchDB(data.couchdbProxy, data.database);
-	render();
+	// console.log('Received configuration.', data);
+	var couchdb = new CouchDB(data.couchdbProxy, data.database);
+	render(couchdb);
 });
--- js/render.js
+++ js/render.js
@@ -1,17 +1,24 @@
-var render = function () { // is called after config.json was loaded
+
+var render = function (couchdb) { // is called after config.json was loaded
 	
-	couchdb.read('meta', function (response, error) {
+	var Theme = Ember.Application.create();
+	
+	Theme.Router.map(function() {
 		
-		if (error) console.log('Error while reading "meta".', error);
-		
-		window.Theme = Ember.Application.create();
-		
-		window.Theme.ApplicationController = Ember.Controller.extend({
-			title: response.title,
-			description: response.description,
-			postsPerPage: response.postsPerPage
+		var routerMapThis = this;
+	
+		couchdb.view('posts', 'paths', function (response, error) {
+			
+			if (error) console.log('Error while getting view "paths" of design document "posts".', error);
+			
+			var paths = response.rows;
+			var length = paths.length;
+			for (var i = 0; i < length; i++) {
+				routerMapThis.route(paths[i].id);
+			}
+			
 		});
-		
+	
 	});
 	
 };
