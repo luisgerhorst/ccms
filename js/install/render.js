62adc77e1e26287f0c17e3de02ee4115e2e0cfd9
Luis Gerhorst <luis@luisgerhorst.de>
2013-04-04 18:40:28 +0200

    DB will have to be created using the CouchDB interface

--- js/install/render.js
+++ js/install/render.js
@@ -1,19 +1,6 @@
 function render(template, couchdb) {
 	
 	template.render('header', function (cb) { cb({}); });
-	
-	template.render('setup-db', function (callback) {
-		
-		$.ajax({
-			url: 'config.json'
-		}).done(function (config) {
-		
-			callback(config);
-		
-		});
-		
-	});
-	
 	template.render('setup-meta', function (cb) { cb({}); });
 	template.render('footer', function (cb) { cb({}); });
 	
