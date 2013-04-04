f57bfe156a2c1d1f48b9bffa33b7ada2227a3363
Luis Gerhorst <luis@luisgerhorst.de>
2013-04-04 18:39:14 +0200

    DB will have to be created using the CouchDB interface

--- js/install/routes.js
+++ js/install/routes.js
@@ -2,60 +2,7 @@ function setRoutes(template, couchdb) {
 	
 	template.route(/^\/$/, [], function () {
 		document.title = 'CCMS';
-		window.location = '#/setup-db'
-	});
-	
-	template.route(/^\/setup-db$/, ['header', 'setup-db', 'footer'], function (views) {
-		
-		var config = views['setup-db'];
-		
-		document.title = 'CCMS';
-		
-		$('#setup-db-login').submit(function () { // on save
-		
-			$.ajax({
-				type: "GET",
-				url: config.couchdbProxy + '/_all_dbs'
-			}).done(function(databases) {
-				
-				databases = JSON.parse(databases);
-				
-				console.log('Received existing DBs.', databases);
-				
-				var exists = false;
-				for (var i = databases.length; i--;) {
-					if (databases[i] == config.database) exists = true;
-				}
-				
-				if (exists) alert('Database does already exist.');
-				else createDB();
-				
-			});
-			
-			function createDB() {
-				
-				var username = $('#setup-db-username').val(), password = $('#setup-db-password').val();
-				
-				$.ajax({
-					type: "PUT",
-					url: config.couchdbProxy + '/' + config.database + '/',
-					beforeSend: function (xhr) {
-						xhr.setRequestHeader('Authorization', 'Basic ' + btoa(username + ':' + password));
-					}
-				}).done(function(res) {
-					window.location = '#/setup-meta'
-				});
-				
-			}
-			
-			return false;
-		
-		});
-		
-		$('#db-reload').click(function () { // on save
-			window.location.reload();
-		});
-		
+		window.location = '#/setup-meta'
 	});
 	
 	template.route(/^\/setup-meta$/, ['header', 'setup-meta', 'footer'], function () {
