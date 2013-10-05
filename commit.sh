d24991707d6e6cf923bd0945337e7145dfd25435
Luis Gerhorst <luis@luisgerhorst.de>
2013-10-06 01:57:29 +0200

    none

--- commit.sh
+++ commit.sh
@@ -4,22 +4,10 @@
 
 git checkout dev
 
-read -p "Files:" filesToCommit
+read -p "Files: " filesToCommit
 git add $filesToCommit -A
 
-read -p "Description:" commitMessage
+read -p "Description: " commitMessage
 git commit -m $commitMessage
 
 git push origin dev
-
-git checkout min
-git merge dev
-
-sh optimize.sh
-
-git add . -A
-git commit -m "optimized files"
-
-git push origin min
-
-git checkout dev
---
+++ merge.sh
@@ -0,0 +1,10 @@
+git checkout min
+
+git merge dev
+
+sh optimize.sh
+
+git add . -A
+git commit -m "optimized files"
+
+git push origin min
