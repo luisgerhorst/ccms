e2fdf9862004c62644b1b906c41e0c2d5c3bbe4d
Luis Gerhorst <luis@luisgerhorst.de>
2013-10-05 15:44:30 +0200

    minify seperator

--- minify.sh
+++ minify.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Minify and merge files for faster page load.
 # dependencies: yuicompressor, cat
 
 # index
@@ -6,4 +7,4 @@ yuicompressor -o etc/database.min.js etc/database.js
 yuicompressor -o etc/theme.min.js etc/theme.js
 yuicompressor -o etc/error.min.js etc/error.js
 yuicompressor -o etc/index/start.min.js etc/index/start.js
-cat etc/libs/jquery.min.js etc/libs/mustache.min.js etc/database.min.js etc/theme.min.js etc/error.min.js etc/index/start.min.js > etc/index/all.js
+cat etc/libs/jquery.min.js minify-seperator.txt etc/libs/mustache.min.js minify-seperator.txt etc/database.min.js minify-seperator.txt etc/theme.min.js minify-seperator.txt etc/error.min.js minify-seperator.txt etc/index/start.min.js > etc/index/all.js
